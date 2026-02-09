d04db2f599b7a7cdba046c102a9257419decf094
Abdullayev <70691153+Abdullayews@users.noreply.github.com>
2026-02-09 19:37:38 +0400

    Update 6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 9):
     for j in range(i + 1, 10):
-        print(i, j, sep="", end=", " if not (i == 8 and j == 9) else "\n")
+        if i == 8 and j == 9:
+            print(i, j, sep="")
+        else:
+            print(i, j, sep="", end=", ")
