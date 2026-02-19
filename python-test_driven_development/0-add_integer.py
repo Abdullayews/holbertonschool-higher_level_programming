db70640d6e4d626209cef8686a303d621dbe9942
Abdullayev <70691153+Abdullayews@users.noreply.github.com>
2026-02-19 22:42:03 +0400

    Update 0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -10,12 +10,12 @@ It handles both integers and floats.
 def add_integer(a, b=98):
     """
     Add two integers.
-    
+
     Cast floats to integers before adding.
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    
+
     return int(a) + int(b)
