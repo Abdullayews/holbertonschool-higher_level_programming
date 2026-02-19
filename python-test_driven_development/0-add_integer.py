e939ebcfb2e999ae974693e52ad1169a60bef3e2
Abdullayev <70691153+Abdullayews@users.noreply.github.com>
2026-02-19 22:38:08 +0400

    Update 0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,21 +1,12 @@
 #!/usr/bin/python3
-"""
-Module for add_integer function.
-
+"""Module for add_integer function.
 This module provides a function to add two integers.
-It handles both integers and floats.
-"""
-
-
+It handles both integers and floats."""
 def add_integer(a, b=98):
-    """
-    Add two integers.
-    
-    Cast floats to integers before adding.
-    """
+    """Add two integers.
+    Cast floats to integers before adding."""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    
     return int(a) + int(b)
