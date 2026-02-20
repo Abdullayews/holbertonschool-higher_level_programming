e6840835b946fc4b43422a429da30eef211bf99a
Abdullayev <70691153+Abdullayews@users.noreply.github.com>
2026-02-21 00:41:53 +0400

    Enhance matrix multiplication with shape validation
    Add checks for empty matrices and raise ValueError for incompatible shapes.

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -15,4 +15,19 @@ def lazy_matrix_mul(m_a, m_b):
     """
     if not isinstance(m_a, list) or not isinstance(m_b, list):
         raise TypeError("Scalar operands are not allowed, use '*' instead")
+
+    if m_a == [[]] or m_b == [[]]:
+        rows_a = len(m_a)
+        cols_a = len(m_a[0]) if m_a else 0
+        rows_b = len(m_b)
+        cols_b = len(m_b[0]) if m_b else 0
+        if m_a == [[]]:
+            raise ValueError(
+                "shapes ({},{}) and ({},{}) not aligned: {} (dim 1) != {} (dim 0)"
+                .format(rows_a, cols_a, rows_b, cols_b, cols_a, rows_b))
+        if m_b == [[]]:
+            raise ValueError(
+                "shapes ({},{}) and ({},{}) not aligned: {} (dim 1) != {} (dim 0)"
+                .format(rows_a, cols_a, rows_b, cols_b, cols_a, rows_b))
+
     return np.matmul(m_a, m_b)
