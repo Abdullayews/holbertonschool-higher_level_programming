253caef479c9b5197ef239fbf60007b8ca8c79a8
Abdullayev <70691153+Abdullayews@users.noreply.github.com>
2026-02-21 00:40:59 +0400

    Update 101-lazy_matrix_mul.py

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -5,14 +5,14 @@ import numpy as np
 
 def lazy_matrix_mul(m_a, m_b):
     """Multiply two matrices using NumPy.
+
     Args:
         m_a (list of lists): First matrix.
         m_b (list of lists): Second matrix.
+
     Returns:
         numpy.ndarray: The result of the matrix multiplication.
-    Raises:
-        TypeError: If m_a or m_b is not a list, not a list of lists,
-                   contains non-integer/float elements, or rows are uneven.
-        ValueError: If m_a or m_b is empty, or matrices can't be multiplied.
     """
+    if not isinstance(m_a, list) or not isinstance(m_b, list):
+        raise TypeError("Scalar operands are not allowed, use '*' instead")
     return np.matmul(m_a, m_b)
