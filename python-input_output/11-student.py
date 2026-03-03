99eb9179ac8f90b041da2d3aa2ee2103f7190069
Abdullayev <70691153+Abdullayews@users.noreply.github.com>
2026-03-04 01:43:55 +0400

    Update 11-student.py

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module that defines a Student class with serialization and deserialization."""
+"""Module that defines a Student class with serialization & deserialization."""
 
 
 class Student:
