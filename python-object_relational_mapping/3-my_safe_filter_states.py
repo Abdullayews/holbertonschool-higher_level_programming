4a23c2ab234eac9cc5254048616c7b3e328f8cd0
Abdullayev <70691153+Abdullayews@users.noreply.github.com>
2026-03-15 19:12:57 +0400

    Update 3-my_safe_filter_states.py

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,32 +1,20 @@
 #!/usr/bin/python3
-"""Displays all values in states table where name matches argument, SQL injection safe"""
+"""
+python script that lists all states from the database hbtn_0e_0_usa with a
+given name and is safe from MySQL injections
+"""
+
 import MySQLdb
-import sys
-
-
-def filter_states():
-    """Takes arguments argv to list from database
-    Safe from MySQL injection using parameterized queries
-    Arguments:
-        argv[1]: mysql username
-        argv[2]: mysql password
-        argv[3]: database name
-        argv[4]: state name
-    """
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE BINARY name = %s \
-                ORDER BY id ASC", (sys.argv[4],))
-    rows = cur.fetchall()
-    for i in rows:
-        print(i)
-    cur.close()
-    db.close()
-
+from sys import argv
 
 if __name__ == "__main__":
-    filter_states()
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
+                   (argv[4],))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
