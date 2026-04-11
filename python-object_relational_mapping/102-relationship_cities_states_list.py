7c1172f646f7abaa2492d6708cb324bfa30ad868
Abdullayev <70691153+Abdullayews@users.noreply.github.com>
2026-04-11 16:21:22 +0400

    Update 102-relationship_cities_states_list.py

--- python-object_relational_mapping/102-relationship_cities_states_list.py
+++ python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
-"""0x0F. Python - Object-relational mapping - task 17. From city
 """
+From city
+"""
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-if __name__ == '__main__':
-    from sys import argv, exit
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-    from relationship_state import Base, State
-    from relationship_city import City
-
-    if len(argv) != 4:
-        exit('Use: 102-relationship_cities_states_list.py <mysql username> '
-             '<mysql password> <database name>')
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/'
-                           '{}'.format(argv[1], argv[2], argv[3]),
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    session = Session(engine)
-    Base.metadata.create_all(engine)  # creates decprecated warning 1681
 
-    for city, state in session.query(City, State).filter(
-            City.state_id == State.id).order_by(City.id).all():
-        print("{}: {} -> {}".format(city.id, city.name, state.name))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    query = session.query(City).order_by(City.id)
+
+    cities = query.all()
+
+    for city in cities:
+        print("{:d}: {:s} -> {:s}".
+              format(city.id, city.name, city.state.name))
+
     session.close()
