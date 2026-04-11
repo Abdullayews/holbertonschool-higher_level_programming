f51f7c8bc28d2db6fc61b6cd1df5dec22fc131a1
Abdullayev <70691153+Abdullayews@users.noreply.github.com>
2026-04-11 16:05:08 +0400

    Refactor city and state creation in main block

--- python-object_relational_mapping/100-relationship_states_cities.py
+++ python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python3
-"""0x0F. Python - Object-relational mapping - task 15. City relationship
 """
+City Relationship
+"""
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-if __name__ == '__main__':
-    from sys import argv, exit
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-    from relationship_state import Base, State
-    from relationship_city import City
-
-    if len(argv) != 4:
-        exit('Use: 100-relationship_states_cities.py <mysql username> '
-             '<mysql password> <database name>')
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/'
-                           '{}'.format(argv[1], argv[2], argv[3]),
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    session = Session(engine)
+
     Base.metadata.create_all(engine)
 
-    new_state = State(name='California')
-    new_city = City(name='San Francisco')
-    new_state.cities.append(new_city)
-    session.add(new_state)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    nstate = "California"
+    ncity = "San Francisco"
+
+    newState = State(name=nstate)
+
+    newCity = City(name=ncity, state=newState)
+
+    session.add(newCity)
+
     session.commit()
+
     session.close()
